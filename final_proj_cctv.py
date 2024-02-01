bfc754d21ffbfe0261e1d939d34b329fa7d800c2
mykangelo <mmykangelo@gmail.com>
2024-02-01 19:07:47 +0800

    Added face and body detection and draw on the screen

--- final_proj_cctv.py
+++ final_proj_cctv.py
@@ -5,6 +5,11 @@ import cv2
 # open the webcam
 cap = cv2.VideoCapture(0)  # 0 corresponds to the default camera (you can change it based on your setup)
 
+# use a face and body dectection function
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
+
 # check if the webcam is opened successfully
 if not cap.isOpened():
     print("Error: Could not open webcam.")
@@ -14,14 +19,23 @@ if not cap.isOpened():
 while True:
     # capture frame-by-frame
     ret, frame = cap.read()
+      
+    # convert the frame into the grayscale image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+    
+    # draw the faces on the screen
+    for (x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0,), 3)
+
 
     # check if the frame was captured successfully
     if not ret:
         print("Error: Failed to capture frame.")
         break
 
-    # cerform any processing on the frame (optional)
-    # e.g., you can add image processing techniques here
+    # perform any processing on the frame (optional)
+    # e.g., add image processing techniques here
 
     # display the frame
     cv2.imshow('CCTV Feed', frame)
